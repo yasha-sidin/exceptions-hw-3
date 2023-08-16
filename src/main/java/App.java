cc6d1d6921d673614b9e5950b16a44ed9bc2be2c
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-08-16 03:28:00 +0300

    Add close method to Client class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,6 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
         Client.requestData();
+        Client.closeClient();
     }
 }
--- src/main/java/Logic/Client.java
+++ src/main/java/Logic/Client.java
@@ -27,4 +27,8 @@ public class Client {
             }
         }
     }
+
+    public static void closeClient() {
+        scanner.close();
+    }
 }
